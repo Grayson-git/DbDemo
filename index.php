5b26482e33ac1589e874f73f9fec60d7c32e5bb4
Grayson-git <graysonlundquist@gmail.com>
2023-02-27 13:36:45 -0700

    modified index file again

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 ?>
 <html lang="en">
 
-<<<<<<< HEAD
+
 <div id="banner">
     <!-- Sidebar Content Here///Specials -->
     <?php  
@@ -54,28 +54,5 @@
 
 </body>
 <?php include INC_PATH.'foot.php';?>
-=======
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 
-<body>
-
-    <?php
-      
-        $_SESSION['username'] = 'Steve';
-    ?>
-
-    <h2>Self Processing Lab</h2>
-
-    <a href="addCatalogItem-lab.php">Add a catalog item</a> <br/>
-    <a href="session1.php">Session 1 page</a><br>
-    <a href="session2.php">Session 2 page</a><br>
-
-</body>
-
->>>>>>> 14225ea7ae4cb2a308945c81bd413423f07dc085
 </html>
