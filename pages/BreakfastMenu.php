828e1a2993ee2354f926d340243206f3405e4af3
Grayson-git <graysonlundquist@gmail.com>
2023-02-27 17:11:21 -0700

    Changed breakfast again

--- pages/BreakfastMenu.php
+++ pages/BreakfastMenu.php
@@ -28,35 +28,36 @@
                     $password = "Csci213+#004";
                     $dbname = "steverq1_grayson";
 
-                // Create connection
-                $conn = new mysqli($servername, $username, $password, $dbname);
-                // Check connection
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                } 
+                    // Create connection
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+                    // Check connection
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    } 
 
-                $sql = "SELECT meals.meal_name, meals.meal_price, meals.meal_description, meals.meal_image, meals.meal_type
-                FROM meals
-                INNER JOIN meal_times
-                ON meals.meal_time_id=meal_times.meal_time_id
-                WHERE meal_times.meal_time_name='Breakfast'";
-                $result = $conn->query($sql);
+                    $sql = "SELECT * FROM meals
+                    INNER JOIN meal_times
+                    ON meals.meal_time_id=meal_times.meal_time_id
+                    WHERE meal_times.meal_time_name='Breakfast'";
+                    $result = $conn->query($sql);
 
-                if ($result->num_rows > 0) {
-                    echo "<table><tr><th>Name</th><th>Price</th><th>Description</th><th>Image</th><th>Type</th></tr>";
-                    // output data of each row
-                    while($row = $result->fetch_assoc()) {
-                        echo "<tr><td>".$row["meal_name"]."</td><td>".$row["meal_price"]."</td><td>".$row["meal_description"]."</td><td>".$row["meal_image"]."</td><td>".$row["meal_type"]."</td></tr>";
+                    if ($result->num_rows > 0) {
+                        echo "<table><tr><th>Name</th><th>Price</th><th>Description</th><th>Image</th><th>Type</th></tr>";
+                        // output data of each row
+                        while($row = $result->fetch_assoc()) {
+                            echo "<tr><td>".$row["meal_name"]."</td><td>".$row["meal_price"]."</td><td>".$row["meal_description"]."</td><td>".$row["meal_image"]."</td><td>".$row["meal_type"]."</td></tr>";
+                        }
+                        echo "</table>";
+                    } else {
+                        echo "0 results";
                     }
-                    echo "</table>";
-                } else {
-                    echo "0 results";
-                }
-                $conn->close();
+                    $conn->close();
                 ?>
 
-</div>
+        </div>
+    </div>
+    
 <?php include INC_PATH.'foot.php';?>
 
-</body>
+
 </html>
